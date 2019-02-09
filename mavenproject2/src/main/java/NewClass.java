d9abff61f088120777a786688b0ba5a5fe578c1d
BOG-A209-E-001 <BOG-A209-E-001@RGH-LS6-01.UMD.LOCAL.EDU>
2019-02-09 08:21:34 -0500

    primer envio

---
+++ mavenproject2/src/main/java/NewClass.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+import org.joda.time.LocalTime;
+/**
+ *
+ * @author BOG-A209-E-001
+ */
+    public class NewClass {
+        public static void main(String[] args) {
+        LocalTime tiempo= new LocalTime();
+        System.out.println("El tiempo es: "+tiempo);
+                        }
+            }
+
